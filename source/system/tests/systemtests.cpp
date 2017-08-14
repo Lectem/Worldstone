6ccc2e9d56b878751caf43c3e3e38fc74534e0d1
Lectem <lectem@gmail.com>
2017-08-14 19:14:51 +0200

    Add excluded files for codecov.io

--- source/system/tests/systemtests.cpp
+++ source/system/tests/systemtests.cpp
@@ -1,6 +1,8 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <FileStream.h>
 #include "doctest.h"
 
-TEST_CASE("dummytest") {
-    CHECK(true);
+TEST_CASE("InexistantFile") {
+    WorldStone::FileStream inexistantFile{"does-not-exist-file"};
+    CHECK(!inexistantFile.is_open());
 }
