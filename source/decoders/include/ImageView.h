7d963e07e06fc4aa5a2526972a0a1e5b5dd5b95d
Lectem <lectem@gmail.com>
2017-12-09 22:38:32 +0100

    Add ImageView+ImageProvider for future use with decoders

---
+++ source/decoders/include/ImageView.h
@@ -0,0 +1,119 @@
+/**@file ImageView.h
+ * Implements image manipulation helpers
+ */
+#pragma once
+
+#include <Vector.h>
+
+namespace WorldStone
+{
+/**A view on an image buffer.
+ * This class can be used to access a buffer of the Color type as if it was an image.
+ * It should be small enough so that you can directly copy the view instead of passing it by
+ * reference. Note that this class does not do bounds checking, and the user is the one responsible
+ * for handling the data correctly.
+ * A good way to abstract an image allocator could be to implement an @ref IImageProvider, which
+ * returns an ImageView.
+ */
+template<class Color>
+struct ImageView
+{
+
+    Color* buffer = nullptr; ///< Pointer to the memory considered as the first pixel
+    size_t width  = 0;       ///< Width of the image, can be dfferent from the one of the buffer
+    size_t height = 0;       ///< Number of scanlines of the image
+    size_t stride = 0;       ///< Actual width of the buffer scanlines
+
+    /**Access the pixel at the given coordinates.
+     * @param x The position in the scanline
+     * @param y The number of the scanline
+     * @return A reference to the pixel of coordinates (x,y)
+     * @warning This function does not perform any bounds checking,
+     *          the only valid inputs verify @code x < width && y < height @endcode
+     */
+    Color& operator()(size_t x, size_t y) { return buffer[x + y * stride]; }
+    /// @overload Color operator()(size_t x, size_t y) const
+    Color operator()(size_t x, size_t y) const { return buffer[x + y * stride]; }
+
+    /// Checks if the view seems to be valid
+    bool isValid() const { return buffer && width && height && width <= stride; }
+
+    bool operator==(const ImageView& rhs) const
+    {
+        return buffer == rhs.buffer && width == rhs.width && height == rhs.height &&
+               stride == rhs.stride;
+    }
+    bool operator!=(const ImageView& rhs) const { return !(*this == rhs); }
+
+    /// Enable conversion from ImageView<Color> to ImageView<const Color>
+    operator ImageView<const Color>() const { return {buffer, width, height, stride}; }
+};
+
+/**An interface of a class that can provide images views
+ * One example would be to reuse the same texture to store multiple images.
+ */
+template<class Color>
+class IImageProvider
+{
+public:
+    /// Returns an ImageView of dimensions width * height to be used by the consumer.
+    virtual ImageView<Color> getNewImage(size_t width, size_t height) = 0;
+    virtual ~IImageProvider() {}
+};
+
+/// A simple image provider that allocates a new buffer for each call to getNewImage()
+template<class Color>
+class SimpleImageProvider : public IImageProvider<Color>
+{
+    struct Image
+    {
+        size_t        width  = 0;
+        size_t        height = 0;
+        Vector<Color> buffer;
+        Image(size_t _width, size_t _height)
+            : width(_width), height(_height), buffer(_width * _height)
+        {
+        }
+    };
+    Vector<Image> images;
+
+public:
+    /// Allocates a new Image of dimensions width * height
+    ImageView<Color> getNewImage(size_t width, size_t height) override
+    {
+        if (!width || !height) return {nullptr};
+        // if (!width || !height) return ImageView<Color>::getInvalidView();
+        images.emplace_back(width, height);
+        return {images.back().buffer.data(), width, height, width};
+    }
+
+    /// @return The number of images allocated
+    size_t getImagesNumber() const { return images.size(); }
+
+    /// @return An ImageView of the imageIndex-th image allocated.
+    ImageView<Color> getImage(size_t imageIndex)
+    {
+        Image& img = images[imageIndex];
+        return {img.buffer.data(), img.width, img.height, img.width};
+    }
+    /// @overload ImageView<const Color> getImage(size_t) const
+    ImageView<const Color> getImage(size_t imageIndex) const
+    {
+        const Image& img = images[imageIndex];
+        return {img.buffer.data(), img.width, img.height, img.width};
+    }
+
+    /**Move an image buffer out of the provider.
+     * @param imageIndex The index of the image to return, in order of allocation.
+     * @note  This means further calls to getImage(imageIndex) will return an invalid ImageView.
+     */
+    Vector<Color> moveImageBuffer(size_t imageIndex)
+    {
+        // We could swap with back() to free memory, but that would invalidate indices.
+        Image& img = images[imageIndex];
+        img.width  = 0;
+        img.height = 0;
+        return std::move(img.buffer);
+    }
+};
+}
---
+++ source/decoders/tests/ImageViewTests.cpp
@@ -0,0 +1,129 @@
+/**
+ * @file ImageViewTests.cpp
+ * @brief Implementation of the tests for ImageView and IImageViewProviders
+ */
+
+#include <Platform.h>
+#include <SystemUtils.h>
+#include <doctest.h>
+#include "ImageView.h"
+
+using WorldStone::Vector;
+using WorldStone::ImageView;
+using WorldStone::SimpleImageProvider;
+
+static_assert(std::is_copy_constructible<ImageView<uint8_t>>::value,
+              "Image view copy must be possible");
+static_assert(std::is_copy_assignable<ImageView<uint8_t>>::value,
+              "Image view copy must be possible");
+static_assert(std::is_convertible<ImageView<uint8_t>, ImageView<const uint8_t>>::value,
+              "Must be able to convert to an ImageView to an ImageView of const values.");
+
+static_assert(std::is_assignable<decltype(ImageView<uint8_t>().operator()(0, 0)), uint8_t>::value,
+              "Must be able to assign values using operator()(size_t,size_t)");
+
+using ConstImageView = const ImageView<uint8_t>;
+static_assert(!std::is_assignable<decltype(ConstImageView().operator()(0, 0)), uint8_t>::value,
+              "Must not be able to assign using operator()(size_t,size_t) on a const ImageView");
+
+TEST_CASE("ImageView")
+{
+    const size_t    size      = 10_z;
+    const uint8_t   initColor = 0;
+    Vector<uint8_t> buffer(size * size, initColor);
+    SUBCASE("Using a valid view")
+    {
+        ImageView<uint8_t> view;
+        CHECK(!view.isValid());
+
+        CHECK(view.buffer == nullptr);
+        view.buffer = buffer.data();
+        CHECK(!view.isValid());
+
+        CHECK(view.width == 0);
+        view.width = size;
+        CHECK(!view.isValid());
+
+        CHECK(view.height == 0);
+        view.height = size;
+        CHECK(!view.isValid());
+
+        CHECK(view.stride == 0);
+        view.stride = view.width;
+        CHECK(view.isValid());
+
+        CHECK(view == ImageView<uint8_t>{buffer.data(), size, size, size});
+        CHECK(view != ImageView<uint8_t>{});
+
+        for (int x = 0; x < size; x++)
+        {
+            for (int y = 0; y < size; y++)
+            {
+                CHECK(view(x, y) == initColor);
+            }
+        }
+        const uint8_t testColor = 5;
+        view(0, 0) = testColor;
+        CHECK(buffer[0] == testColor);
+        CHECK(view(0, 0) == testColor);
+        view(0, 1) = testColor;
+        CHECK(buffer[0 + 1 * size] == testColor);
+        CHECK(view(0, 1) == testColor);
+
+        const ImageView<const uint8_t> viewOnConst = view;
+        CHECK(viewOnConst(0, 1) == testColor);
+    }
+    SUBCASE("Check invalid views")
+    {
+        ImageView<uint8_t> view;
+        CHECK(!view.isValid());
+        // Invalid when any of the fields is null
+        view = {nullptr, size, size, size};
+        CHECK(!view.isValid());
+        view = {buffer.data(), 0, size, size};
+        CHECK(!view.isValid());
+        view = {buffer.data(), size, 0, size};
+        CHECK(!view.isValid());
+        view = {buffer.data(), size, size, 0};
+        CHECK(!view.isValid());
+        // Invalid if stride is more than width
+        view = {buffer.data(), size + 1, size, size};
+        CHECK(!view.isValid());
+    }
+}
+
+TEST_CASE("SimpleImageViewProvider")
+{
+    SimpleImageProvider<uint8_t> imageProvider;
+    CHECK(imageProvider.getImagesNumber() == 0);
+    SUBCASE("Asking for an image of valid dimensions")
+    {
+        ImageView<uint8_t> newImageView = imageProvider.getNewImage(256, 128);
+        CHECK(newImageView.isValid());
+        CHECK(newImageView.width == 256);
+        CHECK(newImageView.height == 128);
+        REQUIRE(imageProvider.getImagesNumber() == 1);
+        CHECK(imageProvider.getImage(0) == newImageView);
+        newImageView = imageProvider.getNewImage(1024, 2048);
+        CHECK(newImageView.width == 1024);
+        CHECK(newImageView.height == 2048);
+        REQUIRE(imageProvider.getImagesNumber() == 2);
+
+        // Make sure we can move data from the provider
+        Vector<uint8_t> imageBuffer = imageProvider.moveImageBuffer(1);
+        CHECK(imageBuffer.size() >= newImageView.width * newImageView.height);
+        CHECK(imageBuffer.data() == newImageView.buffer);
+        // Make sure that the views returned after moving are not valid anymore
+        if (imageProvider.getImagesNumber() == 2) {
+            CHECK(!imageProvider.getImage(1).isValid());
+        }
+    }
+    SUBCASE("Asking for an image of invalid dimensions returns invalid ImageView")
+    {
+        ImageView<uint8_t> invalidImageView = imageProvider.getNewImage(256, 0);
+        CHECK(!invalidImageView.isValid());
+        invalidImageView = imageProvider.getNewImage(0, 256);
+        CHECK(!invalidImageView.isValid());
+        CHECK(imageProvider.getImagesNumber() == 0);
+    }
+}
