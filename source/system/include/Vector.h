911365e16245400d7f21653dab999798321ddabd
Lectem <lectem@gmail.com>
2017-12-09 21:37:29 +0100

    Add a Vector alias for std::vector

---
+++ source/system/include/Vector.h
@@ -0,0 +1,12 @@
+/**@file Vector.h
+ * Just a placeholder to use if we ever want to implement our own vector type
+ */
+
+#pragma once
+#include <vector>
+
+namespace WorldStone
+{
+template<class T>
+using Vector = std::vector<T>;
+}
